d0a6187bf6228961a5604ec5e0b704230fe10936
Ilya <agronat88@icloud.com>
2020-02-16 15:59:11 +0300

    init: create project

---
+++ MultiBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiBrowser
+//
+//  Created by Li on 2/16/20.
+//  Copyright © 2020 Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
